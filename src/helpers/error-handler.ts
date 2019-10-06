a38c86e70cf17bde477580336f9200852d94d3ba
van Hilten Lars <lars.vanhilten@swisscom.com>
2019-10-06 22:39:46 +0200

    Move .env file on build

--- src/helpers/error-handler.ts
+++ src/helpers/error-handler.ts
@@ -3,14 +3,11 @@ export default (error: any) => {
   let message = 'Oops, something went wrong';
 
   if (error.details) {
-    console.log('validator failed');
     code = 400;
     message = error.details[0].message;
   }
 
   if (error.response) {
-    console.log('http failed');
-    console.log(error);
     code = error.response.status;
     message = error.response.data.message || error.response.statusText;
   }
